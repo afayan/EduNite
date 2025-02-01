cf10ecde865a3e66c23f143aa139cc6fff25676a
afayan <ayan032003@gmail.com>
2025-02-01 13:29:33 +0530

    login

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,61 @@
 import dotenv from 'dotenv'
 import express, { json } from 'express'
+import mysql from 'mysql2'
 
 const app = express()
 
-app.use(json)
+dotenv.config()
+app.use(express.json())
+const port = process.env.PORT || 9000
+
+const db = mysql.createConnection({
+    user: 'root',
+    password: process.env.MYSQL,
+    database: 'edunite',
+    host: 'localhost'
+})
+
+
+db.query('create table if not exists users(userid int primary key auto_increment ,email varchar(256) unique not null, username varchar(256) not null, password varchar(300))', [], (err, res)=>{
+
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log("MYSQL connected");
+    }
+})
+
+
+app.post('/api/signup', (req, res)=>{
+
+    const {email, password, username} = req.body
+    
+    db.query('insert into users(email, username, password) values (? , ? , ?);', [email, username, password], (err, result)=>{
+        if (err) {
+            console.log(err);
+            
+            if (err.code == 'ER_DUP_ENTRY') {
+                return res.json({status: false, message : 'Email already in use', error: err}) 
+            }
+            else if (err.code == 'ER_DATA_TOO_LONG') {
+                return res.json({status: false, message : 'Data too long', error: err})             
+            }
+
+            else if (err.code = 'ER_BAD_NULL_ERROR'){
+                return res.json({status : false, message : 'Required  field is missing'})
+            }
+
+            return res.json({status: false, message : 'An error ocurred', error: err})
+        }
+
+
+        return res.json({status: true, message: 'Sign Up complete!'})
+    })
+
+})
+
+app.listen(port, (req, res)=>{
+    console.log("App is running on port "+port);
+})
 
---
+++ frontend/pages/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useState } from "react";
+
+function Login() {
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const nameref = useRef();
+  const passwordref = useRef();
+  const usernameref = useRef();
+  const emailref = useRef();
+  const repeatpasswordref = useRef();
+
+  async function handleSignup() {
+
+    setError('')
+    setSuccess('')
+
+    let password = passwordref.current.value
+    let username = usernameref.current.value.trim()
+    let email = emailref.current.value.trim()
+    let repeat_password = repeatpasswordref.current.value
+
+    if (!password || !username || !email || !repeat_password) {
+        setError('Please fill all data')
+        return
+    }
+
+
+    if (password != repeat_password){
+        setError("Passwords do not match")
+        return
+    }
+
+    const response = await fetch('/api/signup', {
+        method : 'post',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body : JSON.stringify({
+            username: username,
+            email : email,
+            password : password
+        })
+    })
+
+    const data = await response.json()
+
+
+    if (data.status) {
+        //true
+        setSuccess(data.message)
+    }
+
+    else{
+        setError(data.message)
+    }
+
+  }
+
+  return (
+    <div className="loginform">
+      <h2>Sign Up</h2>
+      <input type="text" ref={usernameref} placeholder="username" />
+      <input type="text" ref={emailref} placeholder="email" />
+      <input type="password" ref={passwordref} placeholder="password" />
+      <input type="password" ref={repeatpasswordref} placeholder="repeat password" />
+      <button onClick={handleSignup}>Sign Up</button>
+      <p id="error">{error}</p>
+      <p id="success">{success}</p>
+    </div>
+  );
+}
+
+export default Login;
